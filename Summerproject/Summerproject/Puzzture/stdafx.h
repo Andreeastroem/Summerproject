c5897da0d882c0a469bfd87cc9fee8881147eb2a
André Åström <andreeastroem@gmail.com>
2014-06-14 18:24:07 +0200

    SFML + VS 2013
    SFML linking with vs 2013

---
+++ Summerproject/Summerproject/Puzzture/stdafx.h
@@ -0,0 +1,18 @@
+// stdafx.h
+
+#pragma once
+
+#include <sdkddkver.h>
+
+#include <stdio.h>
+#include <tchar.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include <SFML\Window.hpp>
+#include <SFML\Graphics.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Audio.hpp> 
